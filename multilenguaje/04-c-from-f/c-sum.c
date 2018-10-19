103d935b7c9a5ac7db6ff32e73df004ff6026f65
ilcarbo <agustincarpaneto@gmail.com>
2018-10-19 16:53:55 +0200

    correcciones para compatibilidad

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "c-sum.h"
 
-int sum_abs_(int *in, int num) {
+int sum_abs_(int *in, int *num) {
    int i,sum;
 
-   for (i=0,sum=0; i < num; ++i) {
+   for (i=0,sum=0; i < *num; ++i) {
        sum += abs(in[i]);
    }
    return sum;
