d884d914fdb83a10c3284802069c370a1d94ec81
ilcarbo <agustincarpaneto@gmail.com>
2018-10-22 15:09:39 -0300

    terminado multilenguaje, fijarse bien el txt  de reglas de 05 y los cambios en c-main

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -6,13 +6,13 @@
 
 int main(int argc, char **argv)
 {
-    int data[NUM], num, i;
+    int data[NUM], num, i, sum;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
-
-    printf("sum=%d\n", sum_abs_(data, num));
+    sum_abs_(data, &num, &sum);
+    printf("sum=%d\n", sum_abs_);
     return 0;
 }
